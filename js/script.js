0f79c5bac2c0b0d942f7bb4d53700fc8429c6797
Enzopasqualini <pasqualinienzo095@gmail.com>
2025-09-17 10:38:52 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -308,9 +308,7 @@ document.addEventListener('DOMContentLoaded', () => {
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"></path>
                         </svg>
                     </div>
-                    <a href="index.html" class="flex items-center space-x-2">
                     <h1 class="text-3xl font-bold text-primary">Navio</h1>
-                    </a>
                 </div>
                 <div class="flex items-center space-x-4">
                     <span class="text-gray-700">Olá, ${state.user.fullName}!</span>
